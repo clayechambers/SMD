18c0cb54a50115fb1d09547d4a43027f439c3e33
Clay Chambers <clayechambers@icloud.com>
2019-09-14 20:34:14 -0500

    updates

--- test.py
+++ test.py
@@ -6,12 +6,19 @@ table = smd.xmlSMD2ListofDictionaries(r'SMD\data\smd.xml')
 start_time = time.time()
 
 with open(r'SMD\data\data.txt','w') as file:
+
     for row in table:
+    
         file.write('----------------------------------------------\n')
+    
         for field in row:
+    
             if not row[field] is None:
+    
                 file.write(field + ' = ' + row[field] + '\n')
+    
             else:
+    
                 file.write(field + ' = NONE\n')
                 
 print("--- %s seconds ---" % (time.time() - start_time))
